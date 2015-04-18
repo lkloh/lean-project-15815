3a36d873c1f1df88633fb3f4d68006e6efe68a4d
Lay Kuan Loh <lkloh@Lays-MBP.wv.cc.cmu.edu>
2015-04-17 20:12:27 -0400

    done

--- Proofs/simple_summation.lean
+++ Proofs/simple_summation.lean
@@ -4,15 +4,21 @@ open nat
 
 definition summ : nat → nat
 | summ 0    := 0
-| summ 1    := 1
-| summ(n+1) := summ n + (n+1)
+| summ(n+1) := summ(n) + (n+1)
 
 eval summ 3
 
-theorem summ_form : ∀ n, summ n = divide (n*(n+1)) 2
-| summ_form 0 := show 0 = divide (0*(0+1)) 2, from rfl
-| summ_form 1 := show 1 = divide (1*(1+1)) 2, from rfl
-| summ_form (n+1) := calc
-    summ n + (n+1) = n*(n+1) div 2 + (n+1) : rfl
-         ...       = (n+1)*(n+2) div 2 : rfl
+theorem two : 1+1 = 2 := rfl
 
+theorem summ_form : ∀ n, 2 * summ n = n*(n+1)
+| summ_form 0 :=  calc 
+        2 * 0 = 0 : mul_zero
+        ... = 0 * (0 + 1) : zero_mul
+| summ_form (n+1) := calc
+    2*summ (n+1) = 2*(summ(n)+(n+1)) : rfl
+     ... = 2*summ(n) + 2*(n+1) : mul.left_distrib
+     ... = n*(n+1) + 2*(n+1)   : {summ_form n}
+     ... = (n+2)*(n+1) : mul.right_distrib
+     ... = (n+1)*(n+2) : mul.comm 
+     ... = (n+1)*(n+(1+1)) : two
+     ... = (n+1)*(n+1+1) : add.assoc
