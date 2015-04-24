146930b6f058300999cf9d65bbe1de1f51f832e1
Lay Kuan Loh <lkloh@Lays-MBP.wv.cc.cmu.edu>
2015-04-24 17:35:49 -0400

    next theorem proved

--- Proofs/binomial_isabelle.lean
+++ Proofs/binomial_isabelle.lean
@@ -1,7 +1,7 @@
 import data.nat
 open nat
 
-
+-- These are taken from http://afp.sourceforge.net/browser_info/current/AFP/Girth_Chromatic/Binomial.html
 
 definition binomial : nat → nat → nat
 | binomial 0 0 := 1
@@ -10,3 +10,25 @@ definition binomial : nat → nat → nat
 | binomial(succ n) k := binomial n (k-1) + binomial n k
 
 eval binomial 5 2
+
+-- ****************************************************************** --
+
+--  (n choose 0) = 1 
+
+theorem binomial_n_0 : ∀ n, binomial n 0 = 1
+| binomial_n_0 0 := show binomial 0 0 = 1, from rfl
+| binomial_n_0 (succ n) := show binomial (succ n) 0 = 1, from rfl
+
+
+
+
+
+-- ****************************************************************** --
+
+
+
+
+
+
+
+-- ****************************************************************** --
