a7c2f6d605a4ac8cff9c9f51bd3ff554143049b4
Lay Kuan Loh <lkloh@Lays-MBP.wv.cc.cmu.edu>
2015-05-14 01:14:39 -0400

    saving

--- Proofs/new_proof.lean
+++ Proofs/new_proof.lean
@@ -8,5 +8,22 @@ definition binomial : nat → nat → nat
 | binomial (succ n) (succ k) := binomial n k + binomial n (succ k)
 
 
+theorem binomial_eq_0 {n : ℕ} : ∀{k}, n < k → binomial n k = 0 := 
+nat.induction_on n
+	(take k, assume H : k > 0,
+		obtain k' (H' : k = succ k'), from exists_eq_succ_of_lt H, 
+		calc
+			binomial 0 k = binomial 0 (succ k') : H' 
+					 ... = 0 : rfl)
+	(take n',
+		assume IH : ∀{k}, n' < k → binomial n' k = 0,
+		take k, assume H : succ n' < k,
+		obtain k' (H' : k = succ k'), from exists_eq_succ_of_lt H, 
+		calc
+			binomial (succ n') k = binomial (succ n') (succ k') : H' 
+			... = binomial n' k' + binomial n' (succ k') : rfl
+			... = 0 : sorry)
+
+
 
 
