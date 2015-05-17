b3c29105db78c88da0c79d90057751a01de48942
Lay Kuan Loh <lkloh@Lays-MacBook-Pro.local>
2015-05-16 18:31:50 -0700

    no succ

--- Proofs/fibonacci_isabelle.lean
+++ Proofs/fibonacci_isabelle.lean
@@ -15,10 +15,10 @@ eval fib(5)
 theorem fib_pos : ∀ n, 0 ≤ fib n,
 	fib_pos 0 := show 0 ≤ 0, from le.refl (0), 
 	fib_pos (succ 0) := calc
-		 0 < succ 0 : zero_lt_succ 0
+		 0 ≤ succ 0 : zero_lt_succ 0
 		 ... = 1 : rfl
-	fib_pos (a+2) := calc
-		  0 = 0 + 0             : rfl
-		... < fib (a+1) + 0     : add_lt_add_right (fib_pos (a+1)) 0
-		... < fib (a+1) + fib a : add_lt_add_left  (fib_pos a)     (fib (a+1))
-		... = fib (a+2)         : rfl
+	fib_pos (succ (succ n)) := calc
+		0 = 0 + 0             : rfl
+		... ≤ fib n + 0     : add_le_add_right (fib_pos n) 0
+		... ≤ fib n + fib (succ n) : add_le_add_left  (fib_pos (succ n)) (fib n)
+		... = fib (succ (succ n)) : rfl
