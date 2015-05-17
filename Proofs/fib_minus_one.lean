2209160dc6765a4af166f606411f2fbb487dc1ea
Lay Kuan Loh <lkloh@Lays-MacBook-Pro.local>
2015-05-16 18:18:47 -0700

    two implementations

--- Proofs/fib_minus_one.lean
+++ Proofs/fib_minus_one.lean
@@ -28,8 +28,10 @@ theorem fibminus: ∀ n, sumF(n)+1 = fib(n+2)
 -- ****************************************************************** --
 
 theorem fib_pos : ∀ n, 0 < fib n,
-fib_pos 0 := show 0 < 1, from zero_lt_succ 0, fib_pos 1 := show 0 < 1, from zero_lt_succ 0, fib_pos (a+2) := calc
-  0 = 0 + 0             : rfl
-... < fib (a+1) + 0     : add_lt_add_right (fib_pos (a+1)) 0
-... < fib (a+1) + fib a : add_lt_add_left  (fib_pos a)     (fib (a+1))
-... = fib (a+2)         : rfl
+	fib_pos 0 := show 0 < 1, from zero_lt_succ 0, 
+	fib_pos 1 := show 0 < 1, from zero_lt_succ 0, 
+	fib_pos (a+2) := calc
+		  0 = 0 + 0             : rfl
+		... < fib (a+1) + 0     : add_lt_add_right (fib_pos (a+1)) 0
+		... < fib (a+1) + fib a : add_lt_add_left  (fib_pos a)     (fib (a+1))
+		... = fib (a+2)         : rfl
--- Proofs/fibonacci_isabelle.lean
+++ Proofs/fibonacci_isabelle.lean
@@ -8,13 +8,17 @@ definition fib : nat → nat
 | fib (succ 0) := 1
 | fib (succ (succ n)) := fib n + fib (succ n)
 
+eval fib(5)
+
 -- ****************************************************************** --
 
---  "fib (Suc n) > 0"
-theorem fib_simp: ∀ n, fib (succ n) > 0
-| fib_simp(0) := show fib (succ 0) > 0, from rfl
-| fib_simp(succ (succ n)) := 
-	calc
-		fib (succ (succ n)) = fib n + fib (succ n) : rfl
-		    	...         > 0 + fib (succ n)     : {fib_simp n}
-	
+theorem fib_pos : ∀ n, 0 ≤ fib n,
+	fib_pos 0 := show 0 ≤ 0, from le.refl (0), 
+	fib_pos (succ 0) := calc
+		 0 < succ 0 : zero_lt_succ 0
+		 ... = 1 : rfl
+	fib_pos (a+2) := calc
+		  0 = 0 + 0             : rfl
+		... < fib (a+1) + 0     : add_lt_add_right (fib_pos (a+1)) 0
+		... < fib (a+1) + fib a : add_lt_add_left  (fib_pos a)     (fib (a+1))
+		... = fib (a+2)         : rfl
