df0401d8906be80c3f2599f96437ccb24acd2bba
Lay Kuan Loh <lkloh@Lays-MacBook-Pro.local>
2015-05-17 21:39:48 -0700

    saved

--- Proofs/fibonacci_isabelle.lean
+++ Proofs/fibonacci_isabelle.lean
@@ -39,8 +39,10 @@ nat.induction_on k
 			... = fib (succ 0) * fib (succ n) + fib 0 * fib n : rfl)
 	(take k' IH,
 		calc 
-			fib(n + (succ k') + 1) = fib(succ (n + k') + 1) : add_succ
-				... = fib(succ (succ (n + k'))) : add_one
+			fib(succ(n + (succ k') + 1)) = fib(succ (succ (n + k') + 1)) : add_succ
+				... = fib(succ (succ (succ (n + k')))) : add_one
+				... = fib (succ (n + k')) + fib (succ (succ (n + k'))) : rfl
+				... = 
 				... = fib (succ k' + 1) * fib (n + 1) + fib (succ k') * fib n : sorry)
 
 
