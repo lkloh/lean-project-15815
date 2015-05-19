0d2bbd73c9e35d4d59db47b70930ca02c2add8b2
Lay Kuan Loh <lkloh@Lays-MacBook-Pro.local>
2015-05-18 21:19:58 -0700

    saving

---
+++ report/fibonacci_int.lean
@@ -0,0 +1,41 @@
+import data.nat
+open nat
+-- source: the official tutorial for lean
+
+definition fib : nat → nat
+| fib 0     := 1
+| fib 1     := 1
+| fib (n+2) := fib (n+1) + fib n
+
+definition sumF : nat → nat
+| sumF 0    := 1
+| sumF(n+1) := sumF(n) + fib(n+1)
+
+eval sumF(2)
+
+-- ****************************************************************** --
+
+theorem fibminus: ∀ n, sumF(n)+1 = fib(n+2)
+| fibminus(0) := show 2 = 2, from rfl
+| fibminus(n+1) := calc
+    sumF(n+1)+1 = (sumF(n) + fib(n+1)) + 1 : rfl
+    ... = sumF(n) + (fib(n+1)+1)   : add.assoc
+    ... = sumF(n) + (1+fib(n+1))   : add.comm
+    ... = (sumF(n)+1) + fib(n+1)   : add.assoc
+    ... = fib(n+2) + fib(n+1)      : {fibminus n}
+    ... = fib(n+3)                 : rfl 
+
+-- ****************************************************************** --
+
+theorem fib_pos : ∀ n, 0 < fib n,
+	fib_pos 0 := show 0 < 1, from zero_lt_succ 0, 
+	fib_pos 1 := show 0 < 1, from zero_lt_succ 0, 
+	fib_pos (a+2) := calc
+		  0 = 0 + 0             : rfl
+		... < fib (a+1) + 0     : add_lt_add_right (fib_pos (a+1)) 0
+		... < fib (a+1) + fib a : add_lt_add_left  (fib_pos a)     (fib (a+1))
+		... = fib (a+2)         : rfl
+
+
+-- ****************************************************************** --
+
---
+++ report/summation.lean
@@ -0,0 +1,30 @@
+
+import data.nat
+open nat
+
+definition summ : nat → nat
+| summ 0    := 0
+| summ(n+1) := summ(n) + (n+1)
+
+eval summ 3
+
+-- ****************************************************************** --
+
+theorem two : 1+1 = 2 := rfl
+
+-- ****************************************************************** --
+
+theorem summ_form : ∀ n, 2 * summ n = n*(n+1)
+| summ_form 0 :=  calc 
+        2 * 0 = 0 : mul_zero
+        ... = 0 * (0 + 1) : zero_mul
+| summ_form (n+1) := calc
+    2*summ (n+1) = 2*(summ(n)+(n+1)) : rfl
+     ... = 2*summ(n) + 2*(n+1) : mul.left_distrib
+     ... = n*(n+1) + 2*(n+1)   : {summ_form n}
+     ... = (n+2)*(n+1) : mul.right_distrib
+     ... = (n+1)*(n+2) : mul.comm 
+     ... = (n+1)*(n+(1+1)) : two
+     ... = (n+1)*(n+1+1) : add.assoc
+
+-- ****************************************************************** --
